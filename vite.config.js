4bd09c5237ff5577fcbaafd23393e86262388a29
Ron Madrid <ron.madrid@op2mise.ai>
2026-04-09 04:21:01 +0800

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/reign-piece',
+  base: '/reign-piece/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
